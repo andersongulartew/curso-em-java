621667c0637705cc77a06063b697bdae8650a548
andersongulartew <andersongularte1993@gmail.com>
2024-08-15 22:23:58 -0300

    .

--- src/targettrust_java/javacore2/aula01/Calculos.java
+++ src/targettrust_java/javacore2/aula01/Calculos.java
@@ -2,12 +2,38 @@ package aula01;
 
 public class Calculos {
 
-      public int soma(int a, int b) {
+    // Estanciados
+    public int soma1(int a, int b) {
         return a + b;
     }
 
-        public  int subtrair(int a, int b) {
+    public int subtrair1(int a, int b) {
         return a - b;
     }
+
+        // Não estanciados por serem static
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+
+    public static int soma2(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair2() {
+        return 15 - 6;
+    }
+    public static void resultadoSubtrair2() {
+        int sub = subtrair2();
+        System.out.println(sub);
+    }
+
+
+
+
+
+
+
 
 }
--- src/targettrust_java/javacore2/aula01/PrimeiraClasse.java
+++ src/targettrust_java/javacore2/aula01/PrimeiraClasse.java
@@ -3,33 +3,37 @@ package aula01;
 
 import com.sun.tools.javac.Main;
 
+import static aula01.BemVindo.mostrarSaudacao;
+import static aula01.Calculos.*;
+
 public class PrimeiraClasse {
     public static void main(String[] args) {
 
         System.out.println("Olá Mundo !");
 
+        // MODELO 1
         // Modelos feitos através da clase CALCULO (não static)
         Calculos result = new Calculos();
-        System.out.println(result.soma(5, 8));
-        System.out.println(result.subtrair(10, 6));
+        System.out.println(result.soma1(5, 8));
+        System.out.println(result.subtrair1(10, 6));
 
         System.out.println("-------------------------------");
 
-
-        // Modelo usado atraves de um método static (metodo static só aceita static)
-        int subtracao = subtrair(6, 3);
-        System.out.println("A subtração é: " + subtracao);
-        int somatorio = soma(3,8);
+        // MODELO 2
+        // Método static chamado da classe CALCULOS(nao precisa estanciar)
+        int somatorio = soma2(3, 8);
         System.out.println("A soma é : " + somatorio);
+        int multi = multiplicar(8, 9);
+        System.out.println("A multiplicação é : " + multi);
 
+        System.out.println("-------------------------------");
+
+        // MODELO 3
+        // Para usar este modelo, deve-se criar (metodo static) e chamá-lo em outro (metodo static void)
+        mostrarSaudacao();// método static chamado da classe BemVindo
+        resultadoSubtrair2();// método static chamado da classe Cálculos
     }
 
-    public static int subtrair(int a, int b) {
-        return a - b;
-    }
-          public static int soma(int a, int b) {
-        return a + b;
-    }
 
 
 }
