8c655c85317781c90bdf10c935ece5bd94839785
andersongulartew <andersongularte1993@gmail.com>
2024-08-23 19:27:20 -0300

    .

--- targettrust/javacore/src/aula05/ExampleDoWhile.java
+++ targettrust/javacore/src/aula05/ExampleDoWhile.java
@@ -1,17 +1,16 @@
 package aula05;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-public class ExemploDoWhile {
+public class ExampleDoWhile {
      private static List<Aluno> alunos = new ArrayList(List.of(
             new Aluno("Ana", LocalDate.parse("1992-01-01")),
             new Aluno("Bia", LocalDate.parse("1993-03-15")),
             new Aluno("Leo", LocalDate.parse("1990-05-23")),
-            new Aluno("Roger", LocalDate.parse("1998-31-12"))
+            new Aluno("Roger", LocalDate.parse("1998-12-31"))
     ));
+
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -47,6 +46,28 @@ public class ExemploDoWhile {
         scanner.next();
     }
 
+    public static void deletarAluno() {
+        System.out.println("Deletar Aluno...");
+        System.out.println("Qual a posição deseja deletar ? ");
+        int posicao = scanner.nextInt();
+
+        if(posicao >= alunos.size()) {
+            System.out.println("Tamanho maior que a lista de alunos, será removido o último");
+            posicao = alunos.size();
+        }
+
+        if(posicao <= 0) {
+            System.out.println("Não aceita-se posição negativa ou zero");
+            return;
+        }
+
+        alunos.remove(posicao - 1);
+
+        System.out.println("Aluno removido com sucesso!");
+        scanner.nextLine();
+
+    }
+
     public static void criarAluno() {
         System.out.println("Criar Aluno...");
         System.out.println("Digite o nome do aluno: ");
@@ -60,28 +81,11 @@ public class ExemploDoWhile {
         alunos.add(aluno);
         System.out.println("Aluno criado com sucesso!");
         scanner.nextLine();
-    }
-    public static void deletarAluno() {
-        System.out.println("Deletar Aluno...");
-        System.out.println("Qual a posição deseja deletar ? ");
-        int posicao = scanner.nextInt();
 
-        if(posicao >= alunos.size()) {
-            System.out.println("Tamanho maior que a lista de alunos, será removido o último");
-            posicao = alunos.size();
-        }
-        if(posicao <= 0) {
-            System.out.println("Não aceita-se posição negativa ou zero");
-            return;
-        }
-        alunos.remove(posicao - 1);
-
-        System.out.println("Aluno removido com sucesso!");
-        scanner.nextLine();
     }
 
     public static void clearScreen() {
-        System.out.print("\0033[H\033[2J");
+        System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 }
