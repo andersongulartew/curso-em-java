91ad28eec826a2c02b1c11724e9f7a3aa654fad3
andersongulartew <andersongularte1993@gmail.com>
2024-08-22 23:22:26 -0300

    Adicionado exercícios e exemplos de Java
    Adicionados diversos exercícios e exemplos de código em Java.
    Inclui exercícios sobre loops (for, while, do-while) com diferentes cenários e aplicações práticas.
    Adicionados exemplos de uso de switch-case para tratamento de opções e decisões condicionais.
    Introduz exemplos práticos com enums para demonstrar o uso de tipos enumerados.
    Inclui um exemplo simples de cálculo de IMC (Índice de Massa Corporal) para aplicação de conceitos de programação em um contexto real.
    Utilização de records para criar classes imutáveis de forma concisa e eficiente.

---
+++ targettrust/javacore/src/aula04/ClassificacaoIMC.java
@@ -0,0 +1,19 @@
+package aula04;
+
+public enum ClassificacaoIMC {
+    MAGRO,
+    NORMAL,
+    SOBREPESO,
+
+//    public String getClassificacaoPorExtenso(){
+//        switch (this){
+//            case MAGRO: return "pessoa magro";
+//            case NORMAL: return "pessoa normal";
+//            case SOBREPESO: return "pessoa obeso";
+//
+//        }
+//    }
+
+
+}
+
